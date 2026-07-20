361b5bf4df64194ed264d252e5fd8a27913b4063
fred <sendawulafredrick@gmail.com>
2026-07-20 13:18:23 -0400

    db_url

--- federalism/src_api/fed_api/settings.py
+++ federalism/src_api/fed_api/settings.py
@@ -220,17 +220,34 @@ WSGI_APPLICATION = 'fed_api.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-import os
+# https://docs.djangoproject.com/en/6.0/ref/settings/#databasesimport os
 
 import os
 from pathlib import Path
 
+
+import dj_database_url
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('SUPABASE_DB_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
+
+
+
+
+
+
+"""
 # --- ENVIRONMENT DETECTION ---
 IS_PRODUCTION = os.getenv('IS_PRODUCTION', '').lower() in ('true', '1', 'yes')
 
 # --- BASE DIR ---
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 # --- DATABASE CONFIGURATION ---
 if IS_PRODUCTION:
@@ -258,7 +275,7 @@ else:
         }
     }
 
-
+"""
 """
 # Determine if we are connecting to an external host
 db_host = os.environ.get('DB_HOST', 'db')
